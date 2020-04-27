21b37deaf6e4c488b4e2e6f0f908211026684f5d
Alex Dryden <adryden3@illinois.edu>
2020-04-27 18:34:57 -0400

    populate module's config with skeleton structure

---
+++ config/module.config.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+'view_manager' => [
+        'template_path_stack' => [
+            __DIR__ . '/../view',
+        ],
+    ],
+    'controllers' => [
+        'invokables' => [
+        ],
+    ],
+    'form_elements' => [
+        'factories' => [
+        ],
+    ],
+];
