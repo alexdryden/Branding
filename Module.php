c2b634223c546176596d885b9bbb7c6dc922d8d3
Alex Dryden <adryden3@illinois.edu>
2020-04-27 18:27:25 -0400

    initalize module with required getConfig() function

---
+++ Module.php
@@ -0,0 +1,18 @@
+<?php
+
+use Omeka\Module\AbstractModule;
+
+class Module extends AbstractModule
+{
+/** Module body **/
+
+/**
+* Get this module's configuration array.
+*
+* @return array
+*/
+public function getConfig()
+{
+return include __DIR__ . '/config/module.config.php';
+}
+}
