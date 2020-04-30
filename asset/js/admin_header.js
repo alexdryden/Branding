2f170d51fdda198460704a35d89132d3815649a9
Alex Dryden <adryden3@illinois.edu>
2020-04-30 15:23:19 -0400

    added second branding mark

--- asset/js/admin_header.js
+++ asset/js/admin_header.js
@@ -1,20 +1,35 @@
-window.addEventListener("load", function () {
+function makeImage(src, alt, href){
     let divBranding = document.createElement('div');
     divBranding.classList.add("logo");
     divBranding.classList.add("admin_header");
 
     let aBranding = document.createElement('a');
-    aBranding.setAttribute("href", brandingLogoHref);
+    aBranding.setAttribute("href", href);
 
     let imgBranding = document.createElement('img');
-    imgBranding.setAttribute("alt", brandingLogoAlt);
-    imgBranding.setAttribute("title", brandingLogoAlt);
-    imgBranding.setAttribute("src", brandingLogoSrc);
+    imgBranding.setAttribute("alt", alt);
+    imgBranding.setAttribute("title", alt);
+    imgBranding.setAttribute("src", src);
 
     aBranding.appendChild(imgBranding);
     divBranding.appendChild(aBranding);
 
+    return divBranding;
+
+}
 
 
+
+window.addEventListener("load",
+    function (){
+    let iopnMark = makeImage(brandingLogoIopnScr, brandingLogoIopnAlt, brandingLogoIopnHref);
+    let libraryMark = makeImage(brandingLogoSrc, brandingLogoAlt, brandingLogoHref);
     let header = document.getElementsByTagName("header")[0];
-    header.insertBefore(divBranding, header.childNodes[0]);}, false );
+    header.insertBefore(iopnMark, header.childNodes[0]);
+    header.insertBefore(libraryMark, header.childNodes[0]);
+
+
+    }, false );
+
+
+
