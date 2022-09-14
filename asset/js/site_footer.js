588321ae8c620d039cbe5fbdddefff18a9835b54
Alex Dryden <adryden3@illinois.edu>
2022-09-14 16:50:48 -0400

    restore correct footer content from merge

--- asset/js/site_footer.js
+++ asset/js/site_footer.js
@@ -1,50 +1,59 @@
+let footer_logo_src = $("meta[name='asset:Library_Vert']").attr("content");
 let footer_html = `
 <div class="footer-library">
-    <div class="row">
-        <div class="column footer-library_left" >
-            <div>Main Library Information Desk</div>
-            <div>(217) 333 -2290</div>
-            <div>1408 W. Gregory Dr.</div>
-            <div>Urbana, IL 61801</div>
-        </div>
+                                <div class="row">
+                                                        <div class="column footer-library_right">
+                                                                <p>ABOUT</p>
+                                                                <div>
+                                                                        <ul>
+                                                                                <li><a href="https://iopn.library.illinois.edu/iopn-mission/">About IOPN</a></li>
+                                                                                <li><a href="https://iopn.library.illinois.edu/advisory-board/">Advisory Board</a></li>
+                                                                                <li><a href="https://iopn.library.illinois.edu/policies/">Policies</a></li>
+                                                                                <li><a href="https://iopn.library.illinois.edu/news/">IOPN News</a></li>
+                                                                                <li><a href="https://iopn.library.illinois.edu/contact/">Contact</a></li>
+                                                                                <li><a href="https://iopn.library.illinois.edu/policies/#accessibility">Accessibility</a></li>
+                                                                        </ul>
+                                                                </div>
 
-        <div class="column ">
-            <div class="footer-library_i-mark">
-                <img id="i-mark" usemap="#lib-shared-footer-wordmark-online__map__small" src="`+footer_logo_src +`" alt="University of Illinois Library">
+                                                        </div>
+                                                        <div class="column footer-library_right">
+                                                                <p>PUBLISHING WITHOUT WALLS</p><p>
+                                                                </p><div>
+                                                                        <ul>
+                                                                                <li><a href="https://iopn.library.illinois.edu/publishing-without-walls">About PWW</a></li>
+                                                                                <li><a href="https://iopn.library.illinois.edu/books/pww/about">About PWW</a></li>
+                                                                                <li><a href="https://iopn.library.illinois.edu/books/pww/catalog">PWW Catalog</a></li>
+                                                                                <li><a href="https://pww.afro.illinois.edu">AFRO-PWW Project</a></li>
 
-                <map name="lib-shared-footer-wordmark-online__map__small">
-                    <area shape="rect" coords="0,0,200,40" href="https://illinois.edu" alt="Block I">
-                    <area shape="rect" coords="0,50,300,100" href="https://www.library.illinois.edu" alt="Illinois Library">
-                </map>
+                                                                        </ul>
+                                                                </div>
 
 
-            </div>
-            <div class="footer-library_copyright text-center">
-                <a href="https://www.library.illinois.edu/geninfo/free-and-open-use-of-online-public-domain-reproductions-metadata-and-digitization-requests/">Library Policy on Free and Open Use of Online Public Domain Reproductions</a>
-            </div>
+                                                        </div>
+                                                        <div class="column footer-library_right">
+                                                                <p>WINDSOR &amp; DOWNS</p>
+                                                                <div>
+                                                                        <ul>
+                                                                                <li><a href="https://iopn.library.illinois.edu/windsor-downs-press/">About Windsor and Downs</a></li>
+                                                                                <li><a href="https://iopn.library.illinois.edu/books/windsor-downs/catalog">Windsor and Downs Catalog</a></li>
+                                                                        </ul>
+                                                                </div>
 
-        </div>
 
-        <div class="column footer-library_right">
-            <div>
-                <ul>
-                    <li>
-                        <a href="https://www.vpaa.uillinois.edu/resources/web_privacy">Privacy Policy</a>
-                    </li>
-                    <li>
-                        <a href="https://guides.library.illinois.edu/usersdisabilities">Accessibility</a>
+                                                        </div>
 
-                    </li>
-                    <li>
-                        <a href="https://www.library.illinois.edu/staff/">Library Staff Website</a>
+                                                        <div class="column footer-library_right">
+                                                                <p>IOPN JOURNALS</p>
+                                                                <div>
+                                                                        <ul>
+                                                                                <li><a href="https://iopn.library.illinois.edu/iopn-journals/">About IOPN Journals</a></li>
+                                                                                <li><a href="https://iopn.library.illinois.edu/journals/">IOPN Journals Catalog</a></li>
+                                                                        </ul>
+                                                                </div>
+                                                        </div>
+                                </div>
+                        </div>
 
-                    </li>
-
-                </ul>
-            </div>
-
-        </div>
-</div>
 `;
 
 $(document).ready(function () {
