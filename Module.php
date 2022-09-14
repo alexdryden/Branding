26196f56bc68a1a59ca8883f65100d418c3cc4bf
Alex Dryden <adryden3@illinois.edu>
2022-09-14 11:28:36 -0400

    apply style fixer

--- Module.php
+++ Module.php
@@ -17,42 +17,34 @@ class Module extends AbstractModule
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
-
     }
 
-    public function addAsset(Event $event){
+    public function addAsset(Event $event)
+    {
         $view = $event->getTarget();
         $view->headScript()->appendFile($view->assetUrl('js/branding.js', 'Branding'));
 
 
         if ($this->getServiceLocator()->get('Omeka\Status')->isSiteRequest()) {
-
             $view->headLink()->appendStylesheet($view->assetUrl('css/branding.css', 'Branding'));
             $view->headScript()->appendFile($view->assetUrl('js/site_footer.js', 'Branding'));
             $view->headScript()->appendFile($view->assetUrl('js/site_header.js', 'Branding'));
             echo $view->partial('common/header');
             echo $view->partial('common/footer');
-
-
-
-
         } else {
             echo $view->partial('common/header');
 
             $view->headScript()->prependFile($view->assetUrl('js/admin_header.js', 'Branding'));
             $view->headLink()->appendStylesheet($view->assetUrl('css/branding.css', 'Branding'));
-
         }
     }
 
     public function attachListeners(SharedEventManagerInterface $sharedEventManager)
     {
         $sharedEventManager->attach(
-          '*',
-          'view.layout',
-          [$this, 'addAsset']
+            '*',
+            'view.layout',
+            [$this, 'addAsset']
         );
     }
-
-
 }
