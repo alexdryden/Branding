5b14556254f0bf97fea07d415977d926ad0d92cf
Alex Dryden <adryden3@illinois.edu>
2022-09-14 16:13:27 -0400

    Pass asset location using meta tags to simplify implementation

--- Module.php
+++ Module.php
@@ -22,20 +22,17 @@ class Module extends AbstractModule
     public function addAsset(Event $event)
     {
         $view = $event->getTarget();
-        $view->headScript()->appendFile($view->assetUrl('js/branding.js', 'Branding'));
-
-
         if ($this->getServiceLocator()->get('Omeka\Status')->isSiteRequest()) {
             $view->headLink()->appendStylesheet($view->assetUrl('css/branding.css', 'Branding'));
-            $view->headScript()->appendFile($view->assetUrl('js/site_footer.js', 'Branding'));
+            $view->headMeta()->appendName('asset:library_header_image', $view->assetUrl('img/library_header_image.png', 'Branding'));
+            $view->headMeta()->appendName('asset:iopn_white_comp_trans', $view->assetUrl('img/iopn_white_comp_trans.png', 'Branding'));
+            $view->headMeta()->appendName('asset:Library_Vert', $view->assetUrl('img/Library_Vert.png', 'Branding'));
             $view->headScript()->appendFile($view->assetUrl('js/site_header.js', 'Branding'));
-            echo $view->partial('common/header');
-            echo $view->partial('common/footer');
-        } else {
-            echo $view->partial('common/header');
+            $view->headScript()->appendFile($view->assetUrl('js/site_footer.js', 'Branding'));
 
-            $view->headScript()->prependFile($view->assetUrl('js/admin_header.js', 'Branding'));
+        } else {
             $view->headLink()->appendStylesheet($view->assetUrl('css/branding.css', 'Branding'));
+            $view->headScript()->appendFile($view->assetUrl('js/site_footer.js', 'Branding'));
         }
     }
 
